6e5b95ef0581ca9ac42c44259695e191ac5a53bb
Moritz Schmidt <fusselwurm@gmail.com>
2017-10-01 13:25:53 +0200

    add file logging for headless clients

--- app.js
+++ app.js
@@ -6,13 +6,10 @@ var serveStatic = require('serve-static')
 
 var config = require('./config')
 var setupBasicAuth = require('./lib/setup-basic-auth')
-if (config.bunyanConfig) {
-  var bunyanConfig = require('bunyan-config')(config.bunyanConfig);
-}
 var Manager = require('./lib/manager')
 var Missions = require('./lib/missions')
 var Mods = require('./lib/mods')
-var Logs = require('./lib/logs')
+var Logs = require('./lib/server-log-paths')
 
 var app = express()
 var server = require('http').Server(app)
